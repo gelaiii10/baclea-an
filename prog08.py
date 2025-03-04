9ba37605e43e2864cbc6c62ef9889e84516e7d0e
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-04 00:33:20 -0800

    first commit

---
+++ prog08.py
@@ -0,0 +1,44 @@
+odd_count = 0
+print("You need to input 10 numbers:")
+
+num1 = int(input("Enter Number 1: "))
+if num1 % 2 != 0:
+    odd_count += 1
+
+num2 = int(input("Enter Number 2: "))
+if num2 % 2 != 0:
+    odd_count += 1
+
+num3 = int(input("Enter Number 3: "))
+if num3 % 2 != 0:
+    odd_count += 1
+
+num4 = int(input("Enter Number 4: "))
+if num4 % 2 != 0:
+    odd_count += 1
+
+num5 = int(input("Enter Number 5: "))
+if num5 % 2 != 0:
+    odd_count += 1
+
+num6 = int(input("Enter Number 6: "))
+if num6 % 2 != 0:
+    odd_count += 1
+
+num7 = int(input("Enter Number 7: "))
+if num7 % 2 != 0:
+    odd_count += 1
+
+num8 = int(input("Enter Number 8: "))
+if num8 % 2 != 0:
+    odd_count += 1
+
+num9 = int(input("Enter Number 9: "))
+if num9 % 2 != 0:
+    odd_count += 1
+
+num10 = int(input("Enter Number 10: "))
+if num10 % 2 != 0:
+    odd_count += 1
+
+print(f"The sum of odd numbers is:", odd_count)
