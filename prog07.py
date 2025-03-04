899a4f01cb542a251a6b81e094266033c675e66c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-04 00:31:56 -0800

    first commit

---
+++ prog07.py
@@ -0,0 +1,34 @@
+total_sum = 0
+print("You need to input 10 numbers:")
+
+num1 = int(input("Enter Number 1: "))
+total_sum += num1
+
+num2 = int(input("Enter Number 2: "))
+total_sum += num2
+
+num3 = int(input("Enter Number 3: "))
+total_sum += num3
+
+num4 = int(input("Enter Number 4: "))
+total_sum += num4
+
+num5 = int(input("Enter Number 5: "))
+total_sum += num5
+
+num6 = int(input("Enter Number 6: "))
+total_sum += num6
+
+num7 = int(input("Enter Number 7: "))
+total_sum += num7
+
+num8 = int(input("Enter Number 8: "))
+total_sum += num8
+
+num9 = int(input("Enter Number 9: "))
+total_sum += num9
+
+num10 = int(input("Enter Number 10: "))
+total_sum += num10
+
+print(f"The sum of the entered numbers is:", total_sum)
