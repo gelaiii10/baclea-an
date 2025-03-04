5b2584cdc3a6840f64374b62fe66bbdf3393d393
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-04 00:29:38 -0800

    first commit

---
+++ prog05.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 == 0:
+    print("ERROR: Division by zero is not allowed.")
+else:
+    quotient = num1/num2
+    print("The quotient os the two numbers is:", quotient)
